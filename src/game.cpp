5907d98a24dac0cc8d2075a0f4128cdbe852ac08
lin0qlin <qiulinglin00@gmail.com>
2024-10-24 14:27:15 +0200

    playerInput

--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,27 @@
 #include "Game.h"
+#include <iostream>
+#include <string>
+
+string wordToGuess = "mot"; //à modifier
+string currentGuessState;
+
+void initializeGame()
+{
+    currentGuessState = string(wordToGuess.size(), '_');
+    cout << "Welcome to Hangman! Bienvenu à PENDU!" << endl;
+    cout << "Le mot à deviner est: " << currentGuessState << endl;
+}
+
+char playerInput() 
+{
+    char guess;
+    cout << "Devinez une lettre: ";
+    cin >> guess;
+    return guess;
+}
+
+void playerRight(bool )
+{
+    cout << "Félicitation, c'est correct ";
+
+}
