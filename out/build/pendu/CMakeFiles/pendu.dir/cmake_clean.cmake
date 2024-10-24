7d4b930c431baa478bef19e05b841abf9d9b6d9b
Vincent27_ <vincent.miras03@gmail.com>
2024-10-24 13:20:00 +0200

    mise en place des fichiers src et include

---
+++ out/build/pendu/CMakeFiles/pendu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pendu.dir/main.cpp.o"
+  "CMakeFiles/pendu.dir/main.cpp.o.d"
+  "pendu"
+  "pendu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pendu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
