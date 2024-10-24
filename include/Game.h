9b12553ec9f92ab5c3fd4ba699769e4e86f498d4
Vincent27_ <vincent.miras03@gmail.com>
2024-10-24 14:35:31 +0200

    Ajout de message d'erreur et réssite lors de la devinette

--- include/Game.h
+++ include/Game.h
@@ -3,7 +3,16 @@
 
 class Game{
 public :
-    
+    Game (Mot mot_choisi){
+        nombre_de_vies=6;
+        mot_secret=mot_choisi;
+        indice_trouve={};
+        liste_input{};
+    }
+
+    void Game::envoi_erreur(char lettre_proposée);
+    void Game::envoi_reussite(char lettre_proposée);
+
 
 private:
     int nombre_de_vies;
--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,12 @@
 #include "Game.h"
+#include <iotream>
+
+void Envoi_erreur(char lettre_proposée){
+    std::cout<<"La lettre proposée n'est pas dans le mot";
+    liste_input.push_back(lettre_proposée);
+}
+
+void Game::Envoi_réussite(char lettre_proposée){
+    std::cout<<"La lettre proposée est dans le mot";
+    liste_input.push_back(lettre_proposée);
+}
